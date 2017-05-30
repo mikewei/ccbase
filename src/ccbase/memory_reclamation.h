34131a02ebffbb191f761494e4f1051a1b2739bc
Bin Wei [mikewei] <bin@vip.qq.com>
2017-05-30 16:10:04 +0800

    ADD: HazardPtrReclamation

--- src/ccbase/memory_reclamation.h
+++ src/ccbase/memory_reclamation.h
@@ -30,15 +30,27 @@
 #ifndef MEMORY_RECLAMATION_H_
 #define MEMORY_RECLAMATION_H_
 
+#include <assert.h>
 #include <atomic>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include <algorithm>
+#include <stdexcept>
+#include <type_traits>
+#include "ccbase/common.h"
 #include "ccbase/closure.h"
 #include "ccbase/accumulated_list.h"
 
 namespace ccb {
 
+/* Ref-count based memory relcamation
+ */
 template <class T>
 class RefCountReclamation {
  public:
+  RefCountReclamation() = default;
+
   void ReadLock() {
     ref_count_.fetch_add(1, std::memory_order_seq_cst);
   }
@@ -56,15 +68,27 @@ class RefCountReclamation {
     while (ref_count_.load(std::memory_order_acquire)) {}
     del_func(ptr);
   }
-  void RetireCleanup() { /* do nothing */ }
+  void RetireCleanup() {}
+
+  struct Trait {
+    using ReadLockPointer = std::false_type;
+    using HasRetireCleanup = std::false_type;
+  };
 
  private:
+  CCB_NOT_COPYABLE_AND_MOVABLE(RefCountReclamation);
+
   std::atomic<size_t> ref_count_{0};
 };
 
+
+/* Epoch based memory relcamation
+ */
 template <class T, class ScopeT = T>
 class EpochBasedReclamation {
  public:
+  EpochBasedReclamation() = default;
+
   void ReadLock() {
     ReaderThreadState* state = reader_state_list_.LocalNode();
     state->is_active.store(true, std::memory_order_relaxed);
@@ -96,7 +120,14 @@ class EpochBasedReclamation {
     }
   }
 
+  struct Trait {
+    using ReadLockPointer = std::false_type;
+    using HasRetireCleanup = std::true_type;
+  };
+
  private:
+  CCB_NOT_COPYABLE_AND_MOVABLE(EpochBasedReclamation);
+
   size_t TryReclaim() {
     uint64_t epoch = global_epoch_.load(std::memory_order_seq_cst);
     if (writer_state_.retire_epoch != epoch) {
@@ -145,12 +176,15 @@ class EpochBasedReclamation {
 
     RetireEntry(T* p, ClosureFunc<void(T*)> f = nullptr)
         : ptr(p), del_func(std::move(f)) {}
+    RetireEntry(const RetireEntry&) = delete;
     RetireEntry(RetireEntry&& e) {
       // move may happen when retire_list.emplace_back
       ptr = e.ptr;
       e.ptr = nullptr;
       del_func = std::move(e.del_func);
     }
+    void operator=(const RetireEntry&) = delete;
+    void operator=(RetireEntry&&) = delete;
     ~RetireEntry() {
       if (ptr) {
         if (del_func) del_func(ptr);
@@ -158,7 +192,7 @@ class EpochBasedReclamation {
       }
     }
   };
-  static constexpr size_t kEpochSlots = 4;  // 3 is enough 4 is optimization
+  static constexpr size_t kEpochSlots = 2;  // only need 2 in this impl
   struct WriterThreadState {
     size_t retire_epoch{0};
     std::vector<RetireEntry> retire_lists[kEpochSlots];
@@ -168,24 +202,218 @@ class EpochBasedReclamation {
 
 template <class T, class ScopeT>
 std::atomic<uint64_t> EpochBasedReclamation<T, ScopeT>::global_epoch_{0};
+
 template <class T, class ScopeT> thread_local
 typename EpochBasedReclamation<T, ScopeT>::WriterThreadState
 EpochBasedReclamation<T, ScopeT>::writer_state_;
 
-#if 0
-template <class ScopeT, size_t kHazardNum = 1, size_t kReclaimThreshold = 16>
+
+/* Hazard pointer based memory relcamation
+ */
+template <class T, class ScopeT = T,
+          size_t kHazardPtrNum = 1,
+          size_t kReclaimThreshold = 8>
 class HazardPtrReclamation {
  public:
-  void ReadLock(void*);
-  void ReadUnlock();
-  template <class F> void Retire(void* ptr, F&& del_func);
+  HazardPtrReclamation() = default;
+
+  void ReadLock(T* ptr, size_t index = 0) {
+    assert(index < kHazardPtrNum);
+    ReaderThreadState* state = reader_state_list_.LocalNode();
+    // memory_order_seq_cst is required to garentee that hazard pointer
+    // is visable to all threads before critical-section
+    state->hazard_ptrs[index].store(ptr, std::memory_order_seq_cst);
+  }
+  void ReadUnlock() {
+    ReaderThreadState* state = reader_state_list_.LocalNode();
+    for (auto& hp : state->hazard_ptrs) {
+      // memory_order_release is required when leaving cirtical-section
+      hp.store(nullptr, std::memory_order_release);
+    }
+  }
+  void Retire(T* ptr) {
+    Retire(ptr, nullptr);
+  }
+  template <class F>
+  void Retire(T* ptr, F&& del_func) {
+    writer_state_.retire_list.emplace_back(ptr, std::forward<F>(del_func));
+    if (writer_state_.retire_list.size() >= kReclaimThreshold) {
+      TryReclaim();
+    }
+  }
+  void RetireCleanup() {
+    for (size_t i = 0; !writer_state_.retire_list.empty(); i++) {
+      if (i > 1000UL) {
+        std::this_thread::sleep_for(std::chrono::milliseconds{1});
+      }
+      TryReclaim();
+    }
+  }
+
+  struct Trait {
+    using ReadLockPointer = std::true_type;
+    using HasRetireCleanup = std::true_type;
+  };
 
  private:
-  struct ThreadState {
-    std::atomic<void*> hazard_ptrs[kHazardNum];
+  CCB_NOT_COPYABLE_AND_MOVABLE(HazardPtrReclamation);
+
+  void TryReclaim() {
+    // use thread local allocation as cache
+    static thread_local std::vector<T*> hazard_ptr_vec;
+    hazard_ptr_vec.clear();
+    // collect and sort hazard-pointers
+    reader_state_list_.Travel([&hazard_ptr_vec](ReaderThreadState* state) {
+      for (auto& hp : state->hazard_ptrs) {
+        T* ptr = hp.load(std::memory_order_acquire);
+        if (ptr) hazard_ptr_vec.push_back(ptr);
+      }
+    });
+    std::sort(hazard_ptr_vec.begin(), hazard_ptr_vec.end());
+    // filter out hazard pointers (survivors) from retire list
+    size_t survivors = 0;
+    for (auto& entry : writer_state_.retire_list) {
+      if (std::binary_search(hazard_ptr_vec.begin(),
+                             hazard_ptr_vec.end(), entry.ptr)) {
+        writer_state_.retire_list[survivors++].swap(entry);
+      }
+    }
+    // reclaim all entries except survivors
+    writer_state_.retire_list.resize(survivors);
   }
+
+  // reader state
+  struct ReaderThreadState {
+    std::atomic<T*> hazard_ptrs[kHazardPtrNum];
+  };
+  using ThisType = HazardPtrReclamation<T, ScopeT>;
+  ThreadLocalList<ReaderThreadState, ThisType> reader_state_list_;
+  // writer state
+  struct RetireEntry {
+    T* ptr;
+    ClosureFunc<void(T*)> del_func;
+
+    RetireEntry()
+        : ptr(nullptr), del_func(nullptr) {
+      assert(false);  // never called
+    }
+    RetireEntry(T* p, ClosureFunc<void(T*)> f = nullptr)
+        : ptr(p), del_func(std::move(f)) {}
+    RetireEntry(const RetireEntry&) = delete;
+    RetireEntry(RetireEntry&& e) {
+      // move may happen when retire_list.emplace_back
+      ptr = e.ptr;
+      e.ptr = nullptr;
+      del_func = std::move(e.del_func);
+    }
+    void operator=(const RetireEntry&) = delete;
+    void operator=(RetireEntry&& e) = delete;
+    void swap(RetireEntry& e) {
+      if (this != &e) {
+        std::swap(this->ptr, e.ptr);
+        this->del_func.swap(e.del_func);
+      }
+    }
+    ~RetireEntry() {
+      if (ptr) {
+        if (del_func) del_func(ptr);
+        else delete ptr;
+      }
+    }
+  };
+  struct WriterThreadState {
+    std::vector<RetireEntry> retire_list;
+  };
+  static thread_local WriterThreadState writer_state_;
 };
-#endif
+
+template <class T, class ScopeT,
+          size_t kHazardPtrNum,
+          size_t kReclaimThreshold> thread_local
+typename HazardPtrReclamation<T, ScopeT,
+                              kHazardPtrNum,
+                              kReclaimThreshold>::WriterThreadState
+HazardPtrReclamation<T, ScopeT,
+                     kHazardPtrNum,
+                     kReclaimThreshold>::writer_state_;
+
+
+/* An adapter class for single pointer reclamation
+ */
+template <class T, class Reclamation>
+class PtrReclamationAdapter : private Reclamation {
+ public:
+  PtrReclamationAdapter() = default;
+
+  template <class R = T*>
+  typename std::enable_if<Reclamation::Trait::ReadLockPointer::value,
+                          R>::type
+  ReadLock(std::atomic<T*>* atomic_ptr) {
+    T* ptr;
+    do {
+      ptr = atomic_ptr->load(std::memory_order_seq_cst);
+      Reclamation::ReadLock(ptr, 0);
+    } while (ptr != atomic_ptr->load(std::memory_order_seq_cst));
+    return ptr;
+  }
+
+  template <class R = T*>
+  typename std::enable_if<!Reclamation::Trait::ReadLockPointer::value,
+                          R>::type
+  ReadLock(std::atomic<T*>* atomic_ptr) {
+    Reclamation::ReadLock();
+    return atomic_ptr->load(std::memory_order_acquire);
+  }
+
+  template <class R = T*>
+  typename std::enable_if<Reclamation::Trait::ReadLockPointer::value,
+                          R>::type
+  ReadLock(T** raw_ptr) {
+    T* ptr;
+    do {
+      ptr = *raw_ptr;
+      Reclamation::ReadLock(ptr, 0);
+    } while (ptr != *raw_ptr);
+    return ptr;
+  }
+
+  template <class R = T*>
+  typename std::enable_if<!Reclamation::Trait::ReadLockPointer::value,
+                          R>::type
+  ReadLock(T** raw_ptr) {
+    Reclamation::ReadLock();
+    return *raw_ptr;
+  }
+
+  void ReadUnlock() {
+    Reclamation::ReadUnlock();
+  }
+
+  void Retire(T* ptr) {
+    Reclamation::Retire(ptr);
+  }
+
+  template <class F>
+  void Retire(T* ptr, F&& del_func) {
+    Reclamation::Retire(ptr, std::forward<F>(del_func));
+  }
+
+  template <class R = void>
+  typename std::enable_if<Reclamation::Trait::HasRetireCleanup::value,
+                          R>::type
+  RetireCleanup() {
+    Reclamation::RetireCleanup();
+  }
+
+  template <class R = void>
+  typename std::enable_if<!Reclamation::Trait::HasRetireCleanup::value,
+                          R>::type
+  RetireCleanup() {
+  }
+
+ private:
+  CCB_NOT_COPYABLE_AND_MOVABLE(PtrReclamationAdapter);
+};
 
 }  // namespace ccb
 
--- test/memory_reclamation_test.cc
+++ test/memory_reclamation_test.cc
@@ -5,85 +5,111 @@
 #include "gtestx/gtestx.h"
 #include "ccbase/memory_reclamation.h"
 
-struct SomeType {
-  SomeType() : val(1) {}
-  ~SomeType() { val = 0; }
+class TraceableObj {
+ public:
+  TraceableObj() : val_(1) {}
+  ~TraceableObj() { val_ = 0; }
+
+  int val() const {
+    return val_;
+  }
+  static size_t allocated_objs() {
+    return allocated_objs_;
+  }
 
   static void* operator new(size_t sz) {
-    allocated++;
+    allocated_objs_++;
     return ::operator new(sz);
   }
   static void operator delete(void* ptr, size_t sz) {
-    allocated--;
+    allocated_objs_--;
     ::operator delete(ptr);
   }
-  int val;
-  static std::atomic<size_t> allocated;
+
+ private:
+  int val_;
+  static std::atomic<size_t> allocated_objs_;
 };
 
-std::atomic<size_t> SomeType::allocated{0};
+std::atomic<size_t> TraceableObj::allocated_objs_{0};
 
-using TestTypes = testing::Types<ccb::RefCountReclamation<SomeType>,
-                                 ccb::EpochBasedReclamation<SomeType>>;
+using TestTypes = testing::Types<ccb::RefCountReclamation<TraceableObj>,
+                                 ccb::EpochBasedReclamation<TraceableObj>,
+                                 ccb::HazardPtrReclamation<TraceableObj>>;
 
 template <class RType>
 class MemoryReclamationTest : public testing::Test {
  protected:
   void SetUp() {
-    ASSERT_EQ(0, SomeType::allocated);
+    ASSERT_EQ(0, TraceableObj::allocated_objs());
   }
   void TearDown() {
-    ASSERT_EQ(0, SomeType::allocated);
+    ASSERT_EQ(0, TraceableObj::allocated_objs());
   }
-  RType recl_;
-  SomeType* ptr_{nullptr};
+  ccb::PtrReclamationAdapter<TraceableObj, RType> recl_;
+  TraceableObj* ptr_{nullptr};
 };
 TYPED_TEST_CASE(MemoryReclamationTest, TestTypes);
 
 template <class RType>
-class MemoryReclamationPerfTest : public MemoryReclamationTest<RType> {
+class MemoryReclamationPerfTest : public testing::Test {
  protected:
   void SetUp() {
-    MemoryReclamationTest<RType>::SetUp();
-    auto read_code = [this] {
+    ASSERT_EQ(0, TraceableObj::allocated_objs());
+    auto reader_code = [this] {
       while (!stop_flag_.load(std::memory_order_relaxed)) {
-        this->recl_.ReadLock();
-        for (int i = 0; this->ptr_ && i < 100; i++) {
-          ASSERT_EQ(1, this->ptr_->val);
+        TraceableObj* ptr = this->recl_.ReadLock(&this->ptr_);
+        for (int i = 0; ptr && i < 100; i++) {
+          ASSERT_EQ(1, ptr->val());
         }
         this->recl_.ReadUnlock();
       }
     };
-    for (auto& t : read_tasks_) {
-      t = std::thread(read_code);
+    for (auto& t : reader_tasks_) {
+      t = std::thread(reader_code);
+    }
+    auto writer_code = [this] {
+      while (!stop_flag_.load(std::memory_order_relaxed)) {
+        auto old_ptr = this->ptr_.exchange(new TraceableObj);
+        if (old_ptr) this->recl_.Retire(old_ptr);
+      }
+      this->recl_.RetireCleanup();
+    };
+    for (auto& t : writer_tasks_) {
+      t = std::thread(writer_code);
     }
   }
   void TearDown() {
     stop_flag_.store(true);
-    for (auto& t : read_tasks_) {
+    for (auto& t : reader_tasks_) {
       t.join();
     }
-    this->recl_.Retire(this->ptr_);
+    for (auto& t : writer_tasks_) {
+      t.join();
+    }
+    this->recl_.Retire(this->ptr_.load());
     this->recl_.RetireCleanup();
-    MemoryReclamationTest<RType>::TearDown();
+    ASSERT_EQ(0, TraceableObj::allocated_objs());
   }
-  std::thread read_tasks_[2];
+  std::thread reader_tasks_[2];
+  std::thread writer_tasks_[1];
   std::atomic<bool> stop_flag_{false};
+  ccb::PtrReclamationAdapter<TraceableObj, RType> recl_;
+  std::atomic<TraceableObj*> ptr_{nullptr};
 };
 TYPED_TEST_CASE(MemoryReclamationPerfTest, TestTypes);
 
 TYPED_TEST(MemoryReclamationTest, Simple) {
-  this->ptr_ = new SomeType;
-  ASSERT_EQ(1, SomeType::allocated);
-  this->recl_.ReadLock();
-  ASSERT_EQ(1, this->ptr_->val);
+  this->ptr_ = new TraceableObj;
+  ASSERT_EQ(1, TraceableObj::allocated_objs());
+  TraceableObj* ptr = this->recl_.ReadLock(&this->ptr_);
+  ASSERT_EQ(1, ptr->val());
   this->recl_.ReadUnlock();
-  ASSERT_EQ(1, SomeType::allocated);
-  auto ptr = this->ptr_;
+  ASSERT_EQ(1, TraceableObj::allocated_objs());
   this->ptr_ = nullptr;
   this->recl_.Retire(ptr);
   this->recl_.RetireCleanup();
-  ASSERT_EQ(0, SomeType::allocated);
+  ASSERT_EQ(0, TraceableObj::allocated_objs());
 }
 
 TYPED_TEST(MemoryReclamationTest, Read) {
@@ -93,23 +119,21 @@ TYPED_TEST(MemoryReclamationTest, Read) {
     this->recl_.Retire(ptr);
     this->recl_.RetireCleanup();
   };
-  this->ptr_ = new SomeType;
-  ASSERT_EQ(1, SomeType::allocated);
-  this->recl_.ReadLock();
-  auto ptr = this->ptr_;
+  this->ptr_ = new TraceableObj;
+  ASSERT_EQ(1, TraceableObj::allocated_objs());
+  auto ptr = this->recl_.ReadLock(&this->ptr_);
   std::thread t{deleter};
   for (int i = 0; i < 10; i++) {
-    ASSERT_EQ(1, SomeType::allocated);
-    ASSERT_EQ(1, ptr->val);
+    ASSERT_EQ(1, TraceableObj::allocated_objs());
+    ASSERT_EQ(1, ptr->val());
     usleep(10000);
   }
   this->recl_.ReadUnlock();
   t.join();
-  ASSERT_EQ(0, SomeType::allocated);
+  ASSERT_EQ(0, TraceableObj::allocated_objs());
 }
 
 TYPED_PERF_TEST(MemoryReclamationPerfTest, RetirePerf) {
-  auto old_ptr = this->ptr_;
-  this->ptr_ = new SomeType;
+  auto old_ptr = this->ptr_.exchange(new TraceableObj);
   if (old_ptr) this->recl_.Retire(old_ptr);
 }
