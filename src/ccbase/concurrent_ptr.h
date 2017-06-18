559c6ed3169540229fdd0093b44d197b3ad6bb3e
Bin Wei [mikewei] <bin@vip.qq.com>
2017-06-18 22:52:47 +0800

    FIX: typo

--- src/ccbase/concurrent_ptr.h
+++ src/ccbase/concurrent_ptr.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2017, Bin Wei <bin@vip.qq.com>
+/* Copyright (c) 2016-2017, Bin Wei <bin@vip.qq.com>
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
